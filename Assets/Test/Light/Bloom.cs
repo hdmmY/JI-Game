4b5c742081be02274e0a9251f21f25dd83a846a2
hdmmY <2439826178@qq.com>
2018-03-28 23:43:29 +0800

    Add PostProcessingStack Plugin

---
+++ Assets/Test/Light/Bloom.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bloom : PostEffectsBase
+{
+    /// <summary>
+    /// Down sample the render texture to reduce draw cost.
+    /// </summary>
+    public int m_downSamples;
+    
+    /// <summary>
+    /// Blur iteration times for blur render texture.
+    /// </summary>
+    /// <remarks>
+    /// Higher iteration times will make draw cost higher but a good blur effect.
+    /// </remarks>
+    public int m_blurIteration;
+}
