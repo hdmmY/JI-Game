77a73c616d3e13a8cec1a142bfd3401ca3f8b327
hdmmY <2439846178@qq.com>
2018-04-16 11:15:50 +0800

    Import a state machine plugin
    The state machine plugin is getted from github/Unity3d-Finite-State-Machine. Will use this plugin to develop the boss3 AI.

---
+++ Assets/10.Test/Falcon/Scripts/Boss2Stage1.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Boss.Falcon
+{
+    public class Boss2Stage1 : MonoBehaviour
+    {
+        
+    }
+}
